10bb460c9330d1bece6b310213f7723b35198e1e
ImMarw <jan.mares@student.ossp.cz>
2025-02-03 15:04:28 +0100

    Updated theme files and added features2

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<section class="error-404 not-found text-center py-5">
-    <div class="container">
-        <h1 class="display-4">Page Not Found</h1>
-        <p class="lead">Sorry, we couldn\'t find the page you\'re looking for.</p>
-        <a href="<?php echo home_url(); ?>" class="btn btn-primary">Back to Home</a>
-    </div>
-</section>
-<?php get_footer(); ?>
